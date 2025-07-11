ba713cb29815a9ba56439c6b3a9d60171898e97d
Joshvin Su <joshvin.su@students.mikroskil.ac.id>
2025-07-11 15:07:32 +0700

    memperbaiki edit profile page.dart, menambahkan fitur keburaman pada foto profile menggunakan slider, dan menambahkan fitur outline border color menggunakan slider juga

--- lib/josh/screen/Isi Drawer/edit_profile_page.dart
+++ lib/josh/screen/Isi Drawer/edit_profile_page.dart
@@ -1,7 +1,8 @@
-// lib/josh/screen/edit_profile_page.dart
+// lib/josh/screen/Isi Drawer/edit_profile_page.dart
+import 'dart:ui';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io'; // Untuk File
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
@@ -11,20 +12,30 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  File? _imageFile; // Menyimpan file gambar yang dipilih
+  Uint8List? _imageBytes;
+  XFile? _imageXFile;
+  double _blurValue = 0.0;
+  // State untuk warna border RGB
+  double _redValue = 0.0;
+  double _greenValue = 0.0;
+  double _blueValue = 0.0;
 
   Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
 
-    setState(() {
-      if (pickedFile != null) {
-        _imageFile = File(pickedFile.path);
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Tidak ada gambar yang dipilih.')),
-        );
-      }
-    });
+    if (pickedFile != null) {
+      final imageBytes = await pickedFile.readAsBytes();
+      setState(() {
+        _imageXFile = pickedFile;
+        _imageBytes = imageBytes;
+        _blurValue = 0.0;
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Tidak ada gambar yang dipilih.')),
+      );
+    }
   }
 
   @override
@@ -39,41 +50,151 @@ class _EditProfilePageState extends State<EditProfilePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: _pickImage,
-              child: CircleAvatar(
-                radius: 80,
-                backgroundColor: Colors.grey[300],
-                backgroundImage: _imageFile != null
-                    ? FileImage(_imageFile!) as ImageProvider<Object>?
-                    : const NetworkImage('https://via.placeholder.com/150'), // Placeholder default
-                child: _imageFile == null
-                    ? Icon(
-                        Icons.camera_alt,
-                        size: 50,
-                        color: Colors.grey[700],
-                      )
-                    : null,
-              ),
+            Stack(
+              children: [
+                // Lingkaran profil dengan blur
+                ImageFiltered(
+                  imageFilter:
+                      ImageFilter.blur(sigmaX: _blurValue, sigmaY: _blurValue),
+                  child: GestureDetector(
+                    onTap: _pickImage,
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Colors.grey.shade300,
+                      backgroundImage: _imageBytes != null
+                          ? MemoryImage(_imageBytes!)
+                          : null,
+                      child: _imageBytes == null
+                          ? Icon(
+                              Icons.camera_alt,
+                              size: 50,
+                              color: Colors.grey.shade700,
+                            )
+                          : null,
+                    ),
+                  ),
+                ),
+                // Outline border di atas lingkaran profil
+                Positioned.fill(
+                  child: GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 3.0,
+                          color: Color.fromRGBO(
+                              _redValue.toInt(),
+                              _greenValue.toInt(),
+                              _blueValue.toInt(),
+                              1.0), // Warna border dinamis
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
+            const SizedBox(height: 20),
+            if (_imageBytes != null)
+              Column(
+                children: [
+                  const Text('Tingkat Keburaman Foto'),
+                  Slider(
+                    value: _blurValue,
+                    min: 0.0,
+                    max: 10.0,
+                    divisions: 100,
+                    label: _blurValue.toStringAsFixed(1),
+                    activeColor: Colors.green,
+                    onChanged: (double value) {
+                      setState(() {
+                        _blurValue = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('Outline Border Color'),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Slider(
+                          value: _redValue,
+                          min: 0,
+                          max: 255,
+                          label: 'R: ${_redValue.toInt()}',
+                          activeColor: Colors.red,
+                          onChanged: (double value) {
+                            setState(() {
+                              _redValue = value;
+                            });
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        '${_redValue.toInt()}',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Slider(
+                          value: _greenValue,
+                          min: 0,
+                          max: 255,
+                          label: 'G: ${_greenValue.toInt()}',
+                          activeColor: Colors.green,
+                          onChanged: (double value) {
+                            setState(() {
+                              _greenValue = value;
+                            });
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        '${_greenValue.toInt()}',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Slider(
+                          value: _blueValue,
+                          min: 0,
+                          max: 255,
+                          label: 'B: ${_blueValue.toInt()}',
+                          activeColor: Colors.blue,
+                          onChanged: (double value) {
+                            setState(() {
+                              _blueValue = value;
+                            });
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        '${_blueValue.toInt()}',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _pickImage,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
-                foregroundColor: Colors.black,
+                foregroundColor: Colors.white,
               ),
               child: const Text('Pilih Foto Profil'),
             ),
-            const SizedBox(height: 20),
-            // Anda bisa menambahkan TextField untuk mengedit nama pengguna, email, dll.
-            // TextField(
-            //   decoration: InputDecoration(labelText: 'Nama Pengguna'),
-            // ),
-            // SizedBox(height: 10),
-            // TextField(
-            //   decoration: InputDecoration(labelText: 'Email'),
-            // ),
             const Spacer(),
             ElevatedButton(
               onPressed: () {
@@ -85,7 +206,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
-                foregroundColor: Colors.black,
+                foregroundColor: Colors.white,
                 minimumSize: const Size(double.infinity, 50),
               ),
               child: const Text('Simpan Perubahan'),
